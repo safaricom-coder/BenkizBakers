99f50bcc0d43a0a5285f9971fa99406a9887bcae
malik taylor l-mint <oscarnyamosi2@gmail.com>
2026-04-19 18:03:27 +0300

    signals fix v3

--- main/signals.py
+++ main/signals.py
@@ -1,31 +1,30 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-from .models import CakeCategory
-
-CATEGORIES = [
-    "Birthday Cakes",
-    "Wedding Cakes",
-    "Anniversary Cakes",
-    "Graduation Cakes",
-    "Custom Designer Cakes",
-    "Cupcakes",
-    "Mini Cakes",
-    "Cheesecakes",
-    "Chocolate Cakes",
-    "Red Velvet Cakes",
-    "Fruit Cakes",
-    "Kids Cakes",
-    "Corporate Cakes",
-    "Seasonal Specials"
-]
+from django.apps import apps
 
 @receiver(post_migrate)
 def create_default_categories(sender, **kwargs):
     if sender.name != "main":
         return
 
-    if not CakeCategory._meta.db_table:
-        return
+    CakeCategory = apps.get_model("main", "CakeCategory")
 
-    for c in CATEGORIES:
-        CakeCategory.objects.get_or_create(name=c)
+    categories = [
+        "Birthday Cakes",
+        "Wedding Cakes",
+        "Anniversary Cakes",
+        "Graduation Cakes",
+        "Custom Designer Cakes",
+        "Cupcakes",
+        "Mini Cakes",
+        "Cheesecakes",
+        "Chocolate Cakes",
+        "Red Velvet Cakes",
+        "Fruit Cakes",
+        "Kids Cakes",
+        "Corporate Cakes",
+        "Seasonal Specials"
+    ]
+
+    for category in categories:
+        CakeCategory.objects.get_or_create(name=category)
