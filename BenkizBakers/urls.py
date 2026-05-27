7d9af829f985eb28bb71917372af0f8376f55dfd
malik taylor l-mint <oscarnyamosi2@gmail.com>
2026-05-27 22:54:57 +0300

    changed order of api and free url paths

--- BenkizBakers/urls.py
+++ BenkizBakers/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls')),
-    path('api/',include('benkizapi.urls'))
+    path('api/',include('benkizapi.urls')),
+    path('',include('main.urls'))
 ]
