8cd2aff35c017331930ae6e5f3fdfec8575b2dcb
malik taylor l-mint <oscarnyamosi2@gmail.com>
2026-06-11 21:24:34 +0300

    remove self auth from me

--- benkizapi/views.py
+++ benkizapi/views.py
@@ -418,9 +418,7 @@ def me_view(request):
     profile = UserProfile.objects.filter(user=request.user).first()
 
     return Response({
-        "access": str(refresh.access_token),
-        "refresh": str(refresh),
-        "user": userdata,
+              "user": userdata,
         "profile": UserProfileSerializer(profile).data if profile else None
 
     })
